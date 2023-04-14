5ab23fe3885834d8af56a10f793758d812521150
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-14 15:16:06 -0700

    completed phase 1

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
-  
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value, parent=nil, children=[])
+        @value = value
+        @parent = parent
+        @children = children
+    end
+
+    def parent=(value)
+        if @parent
+            @parent.children.delete(self)
+        end
+        @parent = value
+        if @parent && !value.children.include?(self)
+            value.children << self
+        end
+    end
 end
