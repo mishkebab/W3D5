e91508f743ab3c6f37c974d06e1484f5e1edbf16
misha <mishabansal7@gmail.com>
2023-04-14 15:43:12 -0700

    Add and remove child nodes

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -3,18 +3,31 @@ class PolyTreeNode
     attr_reader :value, :parent, :children
 
     def initialize(value, parent=nil, children=[])
-        @value = value
-        @parent = parent
-        @children = children
+        @value = value 
+        @parent = parent 
+        @children = children 
     end
 
-    def parent=(value)
+    def parent=(new_parent)
         if @parent
             @parent.children.delete(self)
         end
-        @parent = value
-        if @parent && !value.children.include?(self)
-            value.children << self
+        @parent = new_parent
+        if @parent && !new_parent.children.include?(self)
+            new_parent.children << self
         end
     end
+
+    def add_child(random_node)
+        self.children << random_node
+        random_node.parent = self
+    end 
+
+    def remove_child(child_node)
+        if !self.children.include?(child_node)
+            raise "thats not my baby!"
+        end
+        self.children.delete(child_node)
+        child_node.parent = nil 
+    end 
 end
